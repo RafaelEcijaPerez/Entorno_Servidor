9ef6fd62eeca5317ffc1af62005bbdcf8d134040
Rafael Ecija <Rafaeci777@gmail.com>
2024-10-30 10:23:12 +0100

    Create ejercicio7.php

---
+++ T1/ejercicio7.php
@@ -0,0 +1,31 @@
+<?php 
+$name ='Rafael';
+$favotites =['chocolate','Toffee','Fudge',];
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Multimensional Arrays</title>
+        <link rel="stylesheet" href="css/styles.css">
+    </head>
+    <body>
+        <h1>The Candy Store</h1>
+        <h2>Offers</h2>
+        <p>
+            <?php echo $offers[0]['name']; ?>
+            -<?php echo $offers[0]['price']; ?>
+        </p>
+        <p>
+            <?php echo $offers[1]['name']; ?>
+            -<?php echo $offers[2]['price']; ?>
+        </p>
+        <p>
+            <?php echo $offers[2]['name']; ?>
+            -<?php echo $offers[2]['price']; ?>
+        </p>
+        <p>
+            <?php echo $offers[3]['name']; ?>
+            -<?php echo $offers[3]['price']; ?>
+        </p>
+    </body>
+</html>
