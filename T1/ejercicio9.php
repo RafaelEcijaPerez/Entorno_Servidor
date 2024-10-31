e7d59457374de736b8f832b1a1c0ef24aa389381
Rafael Ecija <Rafaeci777@gmail.com>
2024-10-31 09:36:13 +0100

    ejercicio 9

--- T1/ejercicio9.php
+++ T1/ejercicio9.php
@@ -1,3 +1,17 @@
 <?php 
-$prefix ='Thank'
+$prefix ='Thank you';
+$name ='Rafael';
+$mesaje =$prefix .',' . $name;
 ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>String Operator</title>
+        <link rel="stylesheet" href="css/styles.css">
+    </head>
+    <body>
+        <h1>The Candy Store</h1>
+        <h2><?= $name?> Order</h2>
+        <p><?= $mesaje?></p>
+    </body>
+</html>
