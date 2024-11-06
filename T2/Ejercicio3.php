8a41c07fe1a2ff59858a52810ec6ad8818598608
Rafael Ecija <Rafaeci777@gmail.com>
2024-11-06 10:07:00 +0100

    Ejercicio 3

---
+++ T2/Ejercicio3.php
@@ -0,0 +1,18 @@
+<?php 
+$stock =0;
+$mensaje =($stock >0)? 'In stock' :'More stock coming soon';
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>The Candy Store</h1>
+    <h2>Chocolate</h2>
+    <p><?= $mensaje ?></p>
+</body>
+</html>
