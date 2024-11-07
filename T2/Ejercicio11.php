af170e04e2f569cf5d1d84672d8c4284b552e8d7
Rafael Ecija <Rafaeci777@gmail.com>
2024-11-07 10:03:03 +0100

    Ejercicio 11

---
+++ T2/Ejercicio11.php
@@ -0,0 +1,30 @@
+<?php 
+$products = [ 
+    'Toffee' => 2.99, 
+    'Mints' => 1.99, 
+    'Fudge' => 3.49,
+    ]; 
+?>
+<!DOCTYPE html> 
+    <html> 
+        <head> 
+            <title>for Loop Higher Counter</title> 
+            <link rel="stylesheet" href="css/styles.css"> 
+        </head> 
+        <body> 
+            <h2>Price List</h2> 
+            <table> 
+                <tr> 
+                    <th>Item</th> 
+                    <th>Price</th> 
+                </tr> 
+                <?php 
+                foreach ($products as $item => $price) { ?> 
+                    <tr> 
+                        <td><?= $item ?></td> 
+                        <td>$<?= $price ?></td> 
+                    </tr>
+                <?php } ?>
+            </table>
+        </body>
+    </html>
