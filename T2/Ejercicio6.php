3286de76ac80bbdf9b72cd11d7359dff17c8f249
Rafael Ecija <Rafaeci777@gmail.com>
2024-11-07 09:24:13 +0100

    Ejercicio 6

---
+++ T2/Ejercicio6.php
@@ -0,0 +1,22 @@
+<?php 
+$day ='Tuesday';
+$offer =match($day){
+    'Monday' =>'20% off chocolates',
+    'Tuesday' =>'5% off chocolates',
+    'Saturday','Sunday' => '20% off mints',
+    default => '10% off your entire order',
+};
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>match Expression</title>
+</head>
+<body>
+    <h1>The Candy Store</h1>
+    <h2>Offers on <?= $day; ?></h2>
+    <p><?= $offer ?></p>
+</body>
+</html>
