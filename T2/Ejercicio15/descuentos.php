de8025403a9dda700fd3a041a54a58a7dfd81b15
Rafael Ecija <Rafaeci777@gmail.com>
2024-11-12 09:55:08 +0100

    Ejercicio 15

--- T2/Ejercicio15/descuentos.php
+++ T2/Ejercicio15/descuentos.php
@@ -8,18 +8,21 @@ echo "<table border='1'>
             <th>Meses</th>
             <th>Descuento</th>
             <th>Precio Final</th>
+            <th>Ahorro</th>
         </tr>";
 
 for ($meses = 1; $meses < 13; $meses++) {
     // Calculamos el descuento
     $descuento = ($meses >= 6) ? 0.2 : (($meses >= 3) ? 0.1 : 0); // Operador ternario
     $precioFinal = $cuotaMensual * $meses * (1 - $descuento);
+    $ahorro =$cuotaMensual*$meses - $precioFinal;
 
     // Mostramos la fila de la tabla
     echo "<tr>
             <td>$meses</td>
             <td>" . ($descuento * 100) . "%</td>
             <td>" . $precioFinal, "€</td>
+            <td>". $ahorro,"€</td>
           </tr>";
 }
 
